f6a80c56e68af17feebbb222686a0446b3d0f978
Toshit Chawda <toshit307@gmail.com>
2019-07-15 15:23:35 -0700

    dont need any of this stuff

--- custom_types.py
+++ custom_types.py
@@ -1,27 +1,14 @@
-from typing import TYPE_CHECKING, Tuple
+from typing import Tuple
 
-if TYPE_CHECKING:
-    # this is always False at runtime, but this lets us work around cyclic import dependencies
-    from blocks import Block, BlockID
-    from items import Item, ItemStack
-
-    from characters import Player
-    from world import World
-
-    from server import Server, ServerPlayer
-    from world_server import WorldServer
-else:
-    Block = None
-    BlockID = None
-    Item = None
-    ItemStack = None
-
-    Player = None
-    World = None
-
-    Server = None
-    ServerPlayer = None
-    WorldServer = None
+Block = None
+BlockID = None
+Item = None
+ItemStack = None
+Player = None
+World = None
+Server = None
+ServerPlayer = None
+WorldServer = None
 
 iVector = Tuple[int, int, int]
 fVector = Tuple[float, float, float]
