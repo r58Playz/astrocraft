59c15804a3f3cc8f0de49457804c048916ad3432
Toshit Chawda <toshit307@gmail.com>
2020-02-20 09:03:16 -0800

    Update stuff...

--- update.py
+++ update.py
@@ -47,13 +47,13 @@ def startupdate():
     text.insert(END, "Preparing to compile(DO NOT WORRY, it will not take a lot of time)\n")
     os.chdir(str(pathlib.Path.home()) + "/astro-update")
     ans = subprocess.run("cd " + str(pathlib.Path.home()) + "/astro-update" + "&&" + "pipenv run echo This is working", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=dict(os.environ, PIPENV_IGNORE_VIRTUALENVS="1")).stdout
-    text.insert(END, "\n" + ans)
+    text.insert(END, "\n" + str(ans))
     ans = os.popen("pipenv run pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U").read()
-    text.insert(END, "\n" + ans)
+    text.insert(END, "\n" + str(ans))
     prgbar["value"] = 30
     text.insert(END, "\nStarting to compile...\n")
     ans = os.popen("pipenv run python setup.py build").read()
-    text.insert(END, ans)
+    text.insert(END, str(ans))
     prgbar["value"] = 60
     text.insert(END, "\nStarting to copy files...")
     import shutil
